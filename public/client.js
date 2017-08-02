ed25d23b5776358f521c98cf1047c814d1698ce1
System <none>
2017-08-02 16:27:43 +0000

    🍠🗻 Updated with Glitch

--- public/client.js
+++ public/client.js
@@ -1,26 +1,16 @@
-// client-side js
-// run by the browser each time your view template is loaded
+document.querySelector("form").addEventListener("submit", function (event) {
+  event.preventDefault();
 
-// by default, you've got jQuery,
-// add other scripts at the bottom of index.html
-
-$(function() {
-  console.log('hello world :o');
-  
-  $.get('/dreams', function(dreams) {
-    dreams.forEach(function(dream) {
-      $('<li></li>').text(dream).appendTo('ul#dreams');
-    });
-  });
-
-  $('form').submit(function(event) {
-    event.preventDefault();
-    var dream = $('input').val();
-    $.post('/dreams?' + $.param({dream: dream}), function() {
-      $('<li></li>').text(dream).appendTo('ul#dreams');
-      $('input').val('');
-      $('input').focus();
-    });
-  });
-
+  var xhr = new XMLHttpRequest();
+  var url = "/upload";
+  xhr.open("POST", url);
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === XMLHttpRequest.DONE && xhr.status == 200) {
+      var answer = JSON.parse(xhr.response);
+      console.log(answer);
+      
+    }
+  }
+  var formData = new FormData(this);
+  xhr.send(formData);
 });
--- server.js
+++ server.js
@@ -1,22 +1,13 @@
-// init project
-const express = require('express');
+// init the app
+const express = require("express");
 const app = express();
 
+app.use(express.static(__dirname + "/public"));
 
-// http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.get("/", function(req, res) {
+  res.sendFile("./views/index.html");
+})
 
-// http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (request, response) {
-  response.sendFile(__dirname + '/views/index.html');
-});
-
-
-
-
-
-
-// listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+app.listen(process.env.PORT, () => {
+  console.log("Server listens on PORT: " + process.env.PORT);
 });
